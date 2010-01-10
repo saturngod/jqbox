7e10d6f76b72f76742a4f7ad17e22c72dbfe9caa
Saturngod <saturngod@gmail.com>
2010-01-10 16:09:35 +0800

    change onyes and onno

--- js/jq_box.js
+++ js/jq_box.js
@@ -61,14 +61,14 @@
   		$(".jqbox_yes").click(function(){
   			$(".jqbox_innerhtml").fadeOut("slow");
 		  	$(".jqbox_shadow").fadeOut("slow");
-		  	eval(options.onyes+"()");
+		  	eval(options.onyes);
   		});
   		
   		
   		$(".jqbox_no").click(function(){
   			$(".jqbox_innerhtml").fadeOut("slow");
 		  	$(".jqbox_shadow").fadeOut("slow");
-		  	eval(options.onno+"()");
+		  	eval(options.onno);
   		});
 
   		
