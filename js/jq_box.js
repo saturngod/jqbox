079d652f88051e20937451d4225b94016c5583df
Saturngod <saturngod@gmail.com>
2010-09-29 17:58:00 +0800

    fixed for jquery conflit

--- js/jq_box.js
+++ js/jq_box.js
@@ -1,5 +1,5 @@
-(function($){  
- $.fn.jqbox = function(options) {  
+(function(jQuery){  
+ jQuery.fn.jqbox = function(options) {  
   
    var defaults = {  
    html: "",
@@ -18,83 +18,83 @@
     
   };  
     
-  var options = $.extend(defaults, options);  
+  var options = jQuery.extend(defaults, options);  
       
   return this.each(function() { 
   
-  		$(".jqbox_innerhtml").remove();
-		$(".jqbox_shadow").remove();
+  		jQuery(".jqbox_innerhtml").remove();
+		jQuery(".jqbox_shadow").remove();
 		  	 
   		margin_top=(options.height/2)+50;
   		margin_left=(options.width/2)
   		
   		
-  		$("body").append("<div class='jqbox_shadow'></div>");
-  		$("body").append("<div class='jqbox_innerhtml'><div class='jqbox_html_data'>"+options.html+"</div></div>");
+  		jQuery("body").append("<div class='jqbox_shadow'></div>");
+  		jQuery("body").append("<div class='jqbox_innerhtml'><div class='jqbox_html_data'>"+options.html+"</div></div>");
   		if(options.closebtn)
   		{
-  			$(".jqbox_innerhtml").append("<div class='jqbox_close' style='display:none'>&nbsp;</div>");
+  			jQuery(".jqbox_innerhtml").append("<div class='jqbox_close' style='display:none'>&nbsp;</div>");
   		}
   		
   		if(options.alertbox)
   		{
-  			$(".jqbox_innerhtml").append("<div class='jqbox_bottom'><div class='jqbox_ok jqbox_btn'>"+options.oktxt+"</div></div>");
+  			jQuery(".jqbox_innerhtml").append("<div class='jqbox_bottom'><div class='jqbox_ok jqbox_btn'>"+options.oktxt+"</div></div>");
   		}
   		else if(options.confirmbox)
   		{
-  			$(".jqbox_innerhtml").append("<div class='jqbox_bottom'><div class='jqbox_yes jqbox_btn'>"+options.yestxt+"</div><div class='jqbox_no jqbox_btn'>"+options.notxt+"</div></div>");
+  			jQuery(".jqbox_innerhtml").append("<div class='jqbox_bottom'><div class='jqbox_yes jqbox_btn'>"+options.yestxt+"</div><div class='jqbox_no jqbox_btn'>"+options.notxt+"</div></div>");
   		}
   		
   		
   		
   		
-  		//$(".jqbox_innerhtml").css("width",options.width+"px");
-  		//$(".jqbox_innerhtml").css("height",options.height+"px");
-  		$(".jqbox_innerhtml").css("width","1px");
-  		$(".jqbox_innerhtml").css("height","1px");
-  		$(".jqbox_innerhtml").css("margin-top","-50.5px");
-  		$(".jqbox_innerhtml").css("margin-left","-0.5px");
-		$(".jqbox_innerhtml").css("opacity","0");
+  		//jQuery(".jqbox_innerhtml").css("width",options.width+"px");
+  		//jQuery(".jqbox_innerhtml").css("height",options.height+"px");
+  		jQuery(".jqbox_innerhtml").css("width","1px");
+  		jQuery(".jqbox_innerhtml").css("height","1px");
+  		jQuery(".jqbox_innerhtml").css("margin-top","-50.5px");
+  		jQuery(".jqbox_innerhtml").css("margin-left","-0.5px");
+		jQuery(".jqbox_innerhtml").css("opacity","0");
   		
-  		$(".jqbox_shadow").fadeIn();
-  		//$(".jqbox_innerhtml").fadeIn("slow");
-  		$(".jqbox_innerhtml").animate({
+  		jQuery(".jqbox_shadow").fadeIn();
+  		//jQuery(".jqbox_innerhtml").fadeIn("slow");
+  		jQuery(".jqbox_innerhtml").animate({
   							width:options.width+"px",
   							height:options.height+"px",
   							"margin-top":"-"+margin_top+"px",
   							"margin-left":"-"+margin_left+"px",
   							"opacity":1,
   							}, options.speed,function(){
-  								$(".jqbox_close").fadeIn("fase");
+  								jQuery(".jqbox_close").fadeIn("fase");
   							});
   		
   		if(options.closebtn)
   		{
-  			$(".jqbox_close").click(function(){
-				$(".jqbox_innerhtml").fadeOut("slow");
-			  	$(".jqbox_shadow").fadeOut("slow");
+  			jQuery(".jqbox_close").click(function(){
+				jQuery(".jqbox_innerhtml").fadeOut("slow");
+			  	jQuery(".jqbox_shadow").fadeOut("slow");
 			  	
   			});
   		}
   		
-  		$(".jqbox_ok").click(function(){
-  			$(".jqbox_innerhtml").fadeOut("slow");
-		  	$(".jqbox_shadow").fadeOut("slow");
+  		jQuery(".jqbox_ok").click(function(){
+  			jQuery(".jqbox_innerhtml").fadeOut("slow");
+		  	jQuery(".jqbox_shadow").fadeOut("slow");
 		  	
 		  	if(options.onok!="") eval(options.onok);
 		  	
   		});
   		
-  		$(".jqbox_yes").click(function(){
-  			$(".jqbox_innerhtml").fadeOut("slow");
-		  	$(".jqbox_shadow").fadeOut("slow");
+  		jQuery(".jqbox_yes").click(function(){
+  			jQuery(".jqbox_innerhtml").fadeOut("slow");
+		  	jQuery(".jqbox_shadow").fadeOut("slow");
 		  	if(options.onyes!="")	eval(options.onyes);
   		});
   		
   		
-  		$(".jqbox_no").click(function(){
-  			$(".jqbox_innerhtml").fadeOut("slow");
-		  	$(".jqbox_shadow").fadeOut("slow");
+  		jQuery(".jqbox_no").click(function(){
+  			jQuery(".jqbox_innerhtml").fadeOut("slow");
+		  	jQuery(".jqbox_shadow").fadeOut("slow");
 		  	if(options.onno!="") eval(options.onno);
   		});
 
